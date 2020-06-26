92be1db7b47768c28d1cc49a0f9478512552748c
Aman Sharma <amanharitsh123@gmail.com>
2020-06-26 23:16:45 +0530

    One ques remaining

---
+++ 1624.cpp
@@ -0,0 +1,134 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long int lli;
+
+#define all(arr) arr.begin(),arr.end()
+#define f first
+#define s second
+#define debug1(x) cout<<x<<"\n"
+#define debug2(x,y) cout<<x<<" "<<y<<"\n"
+#define debug3(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n"
+#define nl cout<<"\n";
+#define pq priority_queue
+#define inf 0x3f3f3f3f
+#define test cout<<"abcd\n";
+#define pi pair<int,int>
+#define pii pair<int,pi>
+#define pb push_back
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define MOD 1000000007
+#define space ' '
+#define present(a,b) a.find(b)!=a.end()
+
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+template <typename T>
+void input(vector<T> &arr,lli n) {
+  T temp;
+  for(lli i=0;i<n;i++) cin>>temp, arr.push_back(temp);
+}
+
+template <typename T>
+void output(vector<T> arr) {
+  for(auto x:arr) cout<<x<<" ";
+  cout<<endl;
+}
+
+
+template <typename T>
+void input_set(set<T> &arr,lli n) {
+  T temp;
+  for(lli i=0;i<n;i++) cin>>temp, arr.insert(temp);
+}
+
+lli mul(lli a, lli b) {
+  return (a%MOD*b%MOD)%MOD;
+}
+
+lli power(lli a,lli b) {
+  lli ans = 1;
+  while(b > 0) {
+    if(b&1)
+      ans = mul(ans, a);
+    a = mul(a,a);;
+    b >>= 1;
+  }
+  return ans;
+}
+
+set<int> pos, neg, hori, vert;
+int ans=0;
+
+void foo(vector<string> &arr, int x, int count) {
+
+  if(count==0) {
+    ++ans;
+    return;
+  }
+
+  if(x>=8)
+    return;
+
+  for(int i=x; i<8; i++) {
+    for(int j=0; j<8; j++) {
+      int d1=i-j;
+      int d2=8-i-j;
+      if(present(hori, i) or present(vert, j) or present(pos, d1) or present(neg, d2) or arr[i][j]=='*')
+        continue;
+
+      hori.insert(i);
+      vert.insert(j);
+      pos.insert(d1);
+      neg.insert(d2);
+      foo(arr, i+1, count-1);
+      hori.erase(hori.find(i));
+      vert.erase(vert.find(j));
+      pos.erase(pos.find(d1));
+      neg.erase(neg.find(d2)); 
+    }
+  }
+}
+
+void solve() {
+  vector<string> mat;
+  input(mat, 8);
+  foo(mat, 0, 8);
+  cout << ans << endl;
+}
+
+int main() {
+  
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  lli testcases=1;
+  while(testcases--) {
+    solve();
+  }
+}
+
