804ba1dc17e5ff025e897067cca68273d93398d7
Aman Sharma <amanharitsh123@gmail.com>
2020-06-28 13:33:24 +0530

    Ferris Wheel

---
+++ 1084.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long int lli;
+
+#define all(arr) arr.begin(),arr.end()
+#define f first
+#define s second
+#define debug1(x) cout<<x<<"\n"
+#define debug2(x,y) cout<<x<<" "<<y<<"\n"
+#define debug3(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n"
+#define nl cout<<"\n";
+#define pq priority_queue
+#define inf 0x3f3f3f3f
+#define test cout<<"abcd\n";
+#define pi pair<int,int>
+#define pii pair<int,pi>
+#define pb push_back
+#define MOD 1000000007
+
+template <typename T>
+void input(vector<T> &arr,lli n) {
+  T temp;
+  for(lli i=0;i<n;i++) cin>>temp, arr.push_back(temp);
+}
+
+template <typename T>
+void output(vector<T> arr) {
+  for(auto x:arr) cout<<x<<" ";
+  cout<<endl;
+}
+
+
+template <typename T>
+void input_set(set<T> &arr,lli n) {
+  T temp;
+  for(lli i=0;i<n;i++) cin>>temp, arr.insert(temp);
+}
+
+lli mul(lli a, lli b) {
+  return (a%MOD*b%MOD)%MOD;
+}
+
+lli power(lli num,lli base) {
+  lli ans = 1;
+  while(base>0) {
+    if(base&1)
+      ans = mul(ans, num);
+
+    num = mul(num, num);
+    base >>= 1;
+  }
+
+  return ans;
+}
+
+void solve() {
+  int n, m, k;
+  cin >> n >> m >> k;
+  vector<int> apt_size;
+  vector<int> demands;
+  int ans = 0;
+  input(demands, n);
+  input(apt_size, m);
+  sort(all(apt_size));
+  sort(all(demands));
+  int i=0, j=0;
+  while(i<n and j<m) {
+    lli lo=demands[i]-k;
+    lli hi=demands[i]+k;
+    if(apt_size[j]>=lo and apt_size[j]<=hi) {
+      ++ans, ++i, ++j;
+      continue;
+    }
+    if(apt_size[j]>hi)
+      ++i;
+    else 
+      ++j;
+  }
+
+  cout << ans << endl;
+}
+
+int main() {
+  
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  lli testcases=1;
+  while(testcases--) {
+    solve();
+  }
+}
+
