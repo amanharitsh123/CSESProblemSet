9466222ead69c38a3807c01757ad6cab0b7f00b7
Aman Sharma <amanharitsh123@gmail.com>
2020-06-28 11:41:58 +0530

    Leaving Grid Paths for now

--- 1625.cpp
+++ 1625.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 typedef long long int lli;
 
+#define debug(...) fprintf(stderr, __VA_ARGS__), fflush(stderr)
 #define all(arr) arr.begin(),arr.end()
 #define f first
 #define s second
@@ -61,27 +62,93 @@ bool valid(int x, int y) {
   return x>=0 and x<7 and y>=0 and y<7;
 }
 
-void foo(int i, int j, int count) {
+string inp;
+int ma = 0;
+
+
+
+void print_mat() {
+  for(int i=0; i<7; i++) {
+    for(int j=0; j<7; j++)
+      cout << visited[i][j]<<" ";
+    cout << endl;
+  }
+
+  cout << "********************************" << endl;
+}
+void foo(int i, int j, int count, char last_move) {
+  ma = max(ma, count);
   if(count==48) {
     if(i==6 and j==0)
       ++ans;
     return;
   }
 
-  if(!valid(i,j) or visited[i][j])
+  if(!valid(i,j) or visited[i][j] or (i==6 and j==0))
     return;
 
+
   visited[i][j]=true;
-  for(auto x:moves)
+/*  for(auto x:moves)
     foo(i+x.first, j+x.second, count+1);
+*/
+
+  if(inp[count]=='?' || inp[count]=='D') {
+    int x=i+1;
+    int y=j;
+    foo(x, y, count+1, 'D');
+  }
+
+  if(inp[count]=='?' || inp[count]=='U') {
+    int x=i-1;
+    int y=j;
+    if(y!=6)
+      foo(x, y, count+1, 'U');
+ }
+
+  if(inp[count]=='?' || inp[count]=='R') {
+    int x=i;
+    int y=j+1;
+
+    bool possible = true;
+    int ma=-1;
+    while(y<7 and possible)
+      possible = possible and !visited[i][y], y++;
+    ma = y;
+    y=j;
+
+    possible = !possible;
+    bool possible2 = true;
+    while(i+1<7 and y<ma and possible2)
+      possible2 = possible2 and visited[i+1][y], y++;
+
+    y=j+1;
+    //if(!possible)
+      //print_mat();
+    
+    possible = !(possible and possible2);
+    if(x!=6 and possible)
+      foo(x, y, count+1, 'R');
+ }
+
+  if(inp[count]=='?' || inp[count]=='L') {
+    int x=i;
+    int y=j-1;
+    if(i!=0 or last_move!='D')
+      foo(x, y, count+1, 'L');
+  }
+
+
+
   visited[i][j]=false;
 }
 
 void solve() {
-  string inp;
   cin >> inp;
-  foo(0, 0, 0);
+  memset(visited, false, sizeof(visited));
+  foo(0, 0, 0, ' ');
   cout << ans << endl;
+  cout << ma << endl;
 }
 
 int main() {
@@ -91,7 +158,9 @@ int main() {
 
   lli testcases=1;
   while(testcases--) {
+  clock_t z = clock();
     solve();
+debug("Total Time: %.3f\n", (double)(clock() - z) / CLOCKS_PER_SEC);
   }
 }
 
