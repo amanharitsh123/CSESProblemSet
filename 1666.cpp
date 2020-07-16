2225eef21105ccb6299e9ef1b63f5c86ae0bb775
Aman Sharma <amanharitsh123@gmail.com>
2020-07-16 10:06:34 +0530

    Building Roads

---
+++ 1666.cpp
@@ -0,0 +1,137 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long int lli;
+
+#define all(arr) arr.begin(),arr.end()
+#define f first
+#define s second
+#define debug1(x) cout<<x<<"\n"
+#define debug2(x,y) cout<<x<<" "<<y<<"\n"
+#define debug3(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n"
+#define nl cout<<"\n";
+#define pq priority_queue
+#define inf 0x3f3f3f3f
+#define test cout<<"abcd\n";
+#define pi pair<int,int>
+#define pii pair<int,pi>
+#define pb push_back
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define MOD 1000000007
+
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+
+template <typename T>
+void input(vector<T> &arr,lli n) {
+  T temp;
+  for(lli i=0;i<n;i++) cin>>temp, arr.push_back(temp);
+}
+
+template <typename T>
+void output(vector<T> arr) {
+  for(auto x:arr) cout<<x<<" ";
+  cout<<endl;
+}
+
+
+template <typename T>
+void input_set(set<T> &arr,lli n) {
+  T temp;
+  for(lli i=0;i<n;i++) cin>>temp, arr.insert(temp);
+}
+
+lli mul(lli a, lli b) {
+  return (a%MOD*b%MOD)%MOD;
+}
+
+lli power(lli a,lli b) {
+  lli ans = 1;
+  while(b > 0) {
+    if(b&1)
+      ans = mul(ans, a);
+    a = mul(a,a);;
+    b >>= 1;
+  }
+  return ans;
+}
+
+const int N=2e5+1;
+vector<vector<int>> adj(N);
+bool marked[N];
+
+void bfs(int source) {
+  queue<int> q;
+  q.push(source);
+  marked[source]=true;
+  while(q.size()) {
+    int ele=q.front();
+    q.pop();
+    for(auto x:adj[ele]) {
+      if(marked[x])
+        continue;
+      marked[x]=true;
+      q.push(x);
+    }
+  }
+}
+
+void solve() {
+  int n, m, u, v;
+  cin >> n >> m;
+  memset(marked, false, sizeof(marked));
+  for(int i=0; i<m; i++) {
+    cin >> u >> v;
+    adj[u].pb(v);
+    adj[v].pb(u);
+  }
+
+  vector<int> ans;
+  for(int i=1; i<=n; i++) {
+    if(marked[i]==false) {
+      ans.pb(i);
+      bfs(i);
+    }
+  }
+
+  cout << (int) ans.size() - 1 << endl;
+  for(int i=0; i<(int) ans.size()-1; i++) {
+    cout << ans[i] << " " << ans[i+1] << endl;
+  }
+}
+
+int main() {
+  
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  lli testcases=1;
+  while(testcases--) {
+    solve();
+  }
+}
+
